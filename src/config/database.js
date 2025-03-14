439c4ba284b8c646fea6667e164aa42b44dda05a
Renan Marinho <75453387+RenanMarinho809@users.noreply.github.com>
2025-03-14 13:18:45 -0300

    Update database.js
    Escluindo minhas credenciais do mongodb

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/Loja", {
+    await mongoose.connect("", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
